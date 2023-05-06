b411c303f07b33c9b0a7da410415cf9640d5d9cf
VishwasGarg44 <vishwasgarg144@gmail.com>
2023-05-06 16:41:06 +0530

    Modifying parsing functions to identify varius functions.

--- main.cxx
+++ main.cxx
@@ -2,20 +2,23 @@
 
 
 
-std::vector<std::string> parse_args(int argc, char* argv[]) {
-    std::vector<std::string> args;
-    for (int i = 1; i < argc; ++i) {
-        args.push_back(argv[i]);
+void execute_command(int argc, char* argv[]) {
+    std::map<std::string, std::string> command_args;
+    int num_args_to_parse = std::min(argc - 1, 2); // only parse first two arguments
+    for (int i = 1; i <= num_args_to_parse; ++i) {
+        if (i < argc - 1) {
+            command_args[argv[i]] = argv[i + 1];
+            ++i;
+        } else {
+            command_args[argv[i]] = "";
+        }
     }
-    return args;
+    for (const auto& arg : command_args) {
+        std::cout << arg.first << " -> " << arg.second << std::endl;
+    }
 }
 
 int main(int argc, char* argv[]) {
-    std::vector<std::string> args = parse_args(argc, argv);
-    
-    for (const auto& arg : args) {
-        std::cout << arg << " ";
-    }
-
+    execute_command(argc, argv);
     return 0;
 }
