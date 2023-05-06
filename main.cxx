9ec96118d79ee0a84b25e30cc2363294188ea340
VishwasGarg44 <vishwasgarg144@gmail.com>
2023-05-06 15:55:13 +0530

    Added parsing messages.

--- main.cxx
+++ main.cxx
@@ -1,9 +1,21 @@
 #include "main.hxx"
 
-int main (int argc, char* argv[])
-{
+
+
+std::vector<std::string> parse_args(int argc, char* argv[]) {
+    std::vector<std::string> args;
+    for (int i = 1; i < argc; ++i) {
+        args.push_back(argv[i]);
+    }
+    return args;
+}
+
+int main(int argc, char* argv[]) {
+    std::vector<std::string> args = parse_args(argc, argv);
     
-    bool status = init_dir (argv[1]);    
-    
-    return !status;
+    for (const auto& arg : args) {
+        std::cout << arg << " ";
+    }
+
+    return 0;
 }
