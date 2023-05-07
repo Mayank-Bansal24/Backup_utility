bff171fbaad9f56ceeaa40b00292896634f59715
VishwasGarg44 <vishwasgarg144@gmail.com>
2023-05-07 18:53:04 +0530

    Adding add functionality to the backup utility.

--- main.cxx
+++ main.cxx
@@ -2,6 +2,20 @@
 
 
 
+namespace fs = std::filesystem;
+
+void find_path(const std::string& file, std::vector<std::string>& path) {
+    // implementation of git add function
+    std::cout << "Finding path of file: " << file << std::endl;
+    fs::path current_path = fs::current_path();
+    fs::path file_path = current_path / file;
+    if (fs::exists(file_path)) {
+        path.push_back(file_path.string());
+    } else {
+        std::cout << "File not found: " << file << std::endl;
+    }
+}
+
 void execute_command(int argc, char* argv[]) {
     std::map<std::string, std::string> command_args;
     int num_args_to_parse = std::min(argc - 1, 2); // only parse first two arguments
@@ -13,8 +27,18 @@ void execute_command(int argc, char* argv[]) {
             command_args[argv[i]] = "";
         }
     }
+    std::vector<std::string> path;
     for (const auto& arg : command_args) {
-        std::cout << arg.first << " -> " << arg.second << std::endl;
+        std::cout << "Map formed: "<<arg.first << " -> " << arg.second << std::endl;
+        if (arg.first == "path") {
+            find_path(arg.second, path);
+        }
+        else {
+            std::cout << "Unidentified function" << std::endl;
+        }
+    }
+    for (const auto& p : path) {
+        std::cout << "Path: " << p << std::endl;
     }
 }
 
